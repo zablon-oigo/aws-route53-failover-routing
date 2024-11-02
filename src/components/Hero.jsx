eecd10d096a5d1335def5c4fe74cf1a555c716be
zablon <zablonoigo51@gmail.com>
2024-11-02 15:37:49 +0300

    Add hero slider

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,9 @@
-
+import HeroSlider from './HeroSlider'
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <section className="min-h-[748px] relative pt-24 pb-12 flex items-center overflow-hidden lg:px-[50px] lg:pt-24 after:lg:h-[740px] after:lg:w-[740px] after:lg:bg-orange-tertiary after:lg:absolute after:lg:rounded-full after:lg:-right-28
+    after:lg:-top-24 ">
+      <HeroSlider/>
+    </section>
   )
 }
