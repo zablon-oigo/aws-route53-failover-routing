a72fdcf062232f8dd2b83ae271a0f0999f1e8beb
zablon <zablonoigo51@gmail.com>
2024-11-02 15:53:06 +0300

    Add image slider

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,16 +1,36 @@
 import { Swiper,SwiperSlide } from "swiper/react"
-import 'swipe/css';
+import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import '../swiper.css'
 
-import {Pagination, Navigation, Autoplay} from 'swiper'
+// import {Pagination, Navigation, Autoplay} from 'swiper'
 import {heroSlider} from '../data'
 export default function HeroSlider() {
   return (
     <Swiper>
         {heroSlider.map((slide,index)=>{
-            
+            const {title, subtitle, image, buttonText}=slide;
+            return <SwiperSlide key={index} className="py-12 lg:py-16">
+                <div className="container mx-auto text-center lg:text-left">
+                    <div className="">
+                        <div className="">
+                            <h1 className="">
+                                {title}
+                            </h1>
+                            <p className="">
+                                {subtitle}
+                            </p>
+                            <button className="">
+                                {buttonText}
+                            </button>
+                        </div>
+                        <div className="flex-1">
+                            <img src={image.type} alt="" className="" />
+                        </div>
+                    </div>
+                </div>
+            </SwiperSlide>
         })}
     </Swiper>
   )
