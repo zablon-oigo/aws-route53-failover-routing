082fa4f5eea52828858e4fa0f370354a19cb1a7b
zablon <zablonoigo51@gmail.com>
2024-11-03 17:00:52 +0300

    Update swiper module

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -4,18 +4,18 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import '../swiper.css'
 
-import {Pagination, Navigation, Autoplay} from 'swiper'
+import {Pagination, Navigation, Autoplay} from 'swiper/modules';
 import {heroSlider} from '../data'
 export default function HeroSlider() {
   return (
-    <Swiper>
+    <Swiper modules={[Pagination,Navigation,Autoplay]} autoplay={true} loop={true} pagination={{clickable:true,dynamicBullets:true,}}>
         {heroSlider.map((slide,index)=>{
             const {title, subtitle, image, buttonText}=slide;
             return <SwiperSlide key={index} className="py-12 lg:py-16">
                 <div className="container mx-auto text-center lg:text-left">
                     <div className="">
                         <div className="flex-1">
-                            <h1 className="text-blue text-[36px] leading-tigh lg:text-[72px] lg:leading-[98px] font-extrabold mb-4 ">
+                            <h1 className="text-blue text-[36px] leading-tight lg:text-[72px] lg:leading-[98px] font-extrabold mb-4 ">
                                 {title}
                             </h1>
                             <p className="text-base lg:text-[18px] lg:leading-8 max-w-[540px] mb-8 ">
