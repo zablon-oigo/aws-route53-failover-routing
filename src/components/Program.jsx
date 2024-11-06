f11793562dce9f7989a063b8e927f058a5ebb83d
zablon <zablonoigo51@gmail.com>
2024-11-06 18:44:11 +0300

    reduce image size

--- src/components/Program.jsx
+++ src/components/Program.jsx
@@ -2,7 +2,7 @@ import PhoneIcon from '../assets/img/phone.svg';
 export default function Program() {
   return (
     <div className="flex items-baseline justify-end lg:gap-x-10">
-
+                <img src={PhoneIcon} alt="" className="w-[28px] h-[28px] lg:w-[34px] lg:h-[34px]" />
     </div>
   )
 }
