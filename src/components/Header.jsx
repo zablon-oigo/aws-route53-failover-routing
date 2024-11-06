b8c5f36adc98d383cb9b6efdc69bbc014be2dcb4
zablon <zablonoigo51@gmail.com>
2024-11-06 18:39:36 +0300

    Add nav mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,41 @@
 import Logo from '../assets/img/logo.svg';
 import { CgMenuRight } from 'react-icons/cg';
-import Nav from './Nav'
-import NavMobile from './NavMobile'
+import Nav from './Nav';
+import NavMobile from './NavMobile';
 import { useEffect, useState } from 'react';
 import Program from './Program';
 export default function Header() {
-    const[bg,setBg]=useState(false);
-    const[NavMobile,SetNavMobile]=useState(true)
-    useEffect(()=>{
-        window.addEventListener('scroll', ()=>{
-            window.scrollY > 50 ? setBg(true):setBg(false);
-        })
-    })
+  const [bg, setBg] = useState(false);
+  const [isNavMobileOpen, setIsNavMobileOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 50 ? setBg(true) : setBg(false);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className={`${bg?'bg-white py-3 shadow-md':'bg-none py-5'} fixed w-full right-0 left-0 z-10 transition-all duration-300`}>
-               <div className="container mx-auto">
-                <a href="" className="">
-                    <img src={Logo} alt="" className="" />
-                </a>
-                <div className="hidden lg:flex">
-                    <Nav/>
-                </div>
-                <Program/>
-                <div onClick={()=>SetNavMobile(!NavMobile)} className="cursor-pointer lg:hidden">
-                    <CgMenuRight className='text-3xl text-right'/>
-                </div>
-                <div className={`${NavMobile?'h-[260px]':''} fixed bg-yellow shadow-lg left-0 w-full top-[78px] h-full overflow-hidden transition-all`}>
-                    <NavMobile/>
-                </div>
-               </div>
+    <header className={`${bg ? 'bg-white py-3 shadow-md' : 'bg-none py-5'} fixed w-full right-0 left-0 z-10 transition-all duration-300`}>
+      <div className="container flex items-center justify-between mx-auto">
+        <a href="/" className="flex-shrink-0">
+          <img src={Logo} alt="Logo" className="h-10" />
+        </a>
+        <div className="hidden lg:flex">
+          <Nav />
+        </div>
+        <Program />
+        <div onClick={() => setIsNavMobileOpen(!isNavMobileOpen)} className="cursor-pointer lg:hidden">
+          <CgMenuRight className="text-3xl text-right" />
+        </div>
+        <div
+          className={`${isNavMobileOpen ? 'h-[260px]' : 'h-0'} lg:hidden fixed bg-yellow shadow-lg left-0 w-full top-[78px] overflow-hidden transition-all duration-300 ease-in-out`}>
+          <NavMobile />
+        </div>
+      </div>
     </header>
-  )
+  );
 }
