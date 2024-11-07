1ba66e018230db0aee09091d9f991911ee2eff58
zablon <zablonoigo51@gmail.com>
2024-11-07 23:04:22 +0300

    Add context info

--- src/components/Program.jsx
+++ src/components/Program.jsx
@@ -3,6 +3,15 @@ export default function Program() {
   return (
     <div className="flex items-baseline justify-end lg:gap-x-10">
                 <img src={PhoneIcon} alt="" className="w-[28px] h-[28px] lg:w-[34px] lg:h-[34px]" />
+                <div className="lg:test-[23px] text-blue font-extrabold ">+ 123 456 789 </div>
+                <div className="">
+                  <p className="hidden lg:flex">
+                    Opening Hours: Mon - Sun: 10am - 6pm
+                  </p>
+                  <p className="text-right lg:hidden">
+                    Opening Hours: Mon - Sun: 10am - 6pm
+                  </p>
+                </div>
     </div>
   )
 }
