9d1318c3271922b087cf6df2c73f2a2a3612ada7
zablon <zablonoigo51@gmail.com>
2024-11-08 18:57:23 +0300

    Add apointment component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero"
 import Header from './components/Header'
+import Appointment from "./components/Appointment"
 function App() {
   return (
     <>
     <div className="w-full max-w-[1440px] mx-auto bg-orange-quaternary relative">
       <Header/>
     <Hero/>
+    <Appointment/>
     <div className="h-[2000px]">
       
     </div>
