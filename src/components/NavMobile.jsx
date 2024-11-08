b4c8ffbd2ff0cef20fd511914db193a8039e7e93
zablon <zablonoigo51@gmail.com>
2024-11-08 18:52:04 +0300

    Add nav mobile component

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -1,7 +1,20 @@
+import { navigation } from "../Data";
 
 
 export default function NavMobile() {
   return (
-    <div>NavMobile</div>
+    <nav className="h-full">
+      <ul className="flex flex-col items-center justify-center h-full space-y-5">
+        {navigation.map((item,index)=>{
+          return(
+            <li className="font-medium capitalize text-blue" key={index}>
+              <a href={item.href} className="">
+                {item.name}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
   )
 }
