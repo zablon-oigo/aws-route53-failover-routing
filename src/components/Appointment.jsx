7458e5e41f5282a406a6501247130b03e5159a59
zablon <zablonoigo51@gmail.com>
2024-11-09 06:00:11 +0300

    Add appointment form

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -1,7 +1,17 @@
+import DogImg from '../assets/img/dogs/dog-appointment.png'
 export default function Appointment() {
   return (
     <div className="">
-
+           <div className="">
+            <div className="">
+              <img src={DogImg} alt="" className=''/>
+            </div>
+            <form action="" className="">
+              <h2 className="">
+                Get an appointment
+              </h2>
+            </form>
+           </div>
     </div>
   )
 }
