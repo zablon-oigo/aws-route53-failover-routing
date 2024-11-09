757b4fbd8a33b3fd8e86970b572b41ee861ff198
zablon <zablonoigo51@gmail.com>
2024-11-09 06:06:09 +0300

    Add flex box

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -6,13 +6,13 @@ export default function Appointment() {
             <div className="hidden w-full max-w-[790px] mx-auto lg:flex justify-center">
               <img src={DogImg} alt="" className=''/>
             </div>
-            <form action="" className="bg-yellow py-8 px-6 w-full max-w-[790px] h-[500px]">
+            <form action="" className="flex flex-col bg-yellow py-8 px-6 w-full max-w-[790px] h-[500px] mx-auto lg:-mt-5 rounded-[60px] text-center">
               <h2 className="">
                 Get an appointment
               </h2>
-              <input type="text" className="" />
-              <input type="text" className="" />
-              <input type="text" className="" />
+              <input type="text" className="input-control" />
+              <input type="text" className="input-control" />
+              <input type="text" className="input-control" />
             </form>
            </div>
     </div>
