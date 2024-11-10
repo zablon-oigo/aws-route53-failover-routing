bce219b231e19884aa85278f056f2450be9e558f
zablon <zablonoigo51@gmail.com>
2024-11-10 18:19:06 +0300

    Add diff price package

--- src/components/Prices.jsx
+++ src/components/Prices.jsx
@@ -1,7 +1,13 @@
 import Bundles from "./Bundles"; 
 import { bundleData } from '../Data';
+import { useEffect, useState } from "react";
 
 export default function Prices() {
+  const [index,setIndex]=useState(0)
+  const[bundles,setBundles]=useState([])
+  useEffect(()=>{
+    setBundles(bundleData[0].services)
+  }, [])
   return (
     <section className="py-12 lg:py-24">
       <div className="container mx-auto">
@@ -12,20 +18,25 @@ export default function Prices() {
         </div>
         
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-[30px]">
-          {bundleData.map((item, index) => {
+          {bundleData.map((item, idx) => {
             const { name, image, dogCategory } = item;
             
             return (
-              <div key={name} className="">
+              <div key={idx} onClick={()=>setIndex(idx)} className="text-center cursor-pointer">
                 <div className="mb-2 transition-all duration-300 lg:mb-8 hover:scale-105">
                   <img src={image.type} alt={`Image for ${name}`} className="w-full" />
                 </div>
                 <h3 className="font-semibold capitalize lg:text-2xl text-blue">{name}</h3>
-                <div className="">{dogCategory}</div>
+                <div className={`${index === idx ? 'border-b-4 border-orange transition-all after:lg:w-5 after:lg:h-5 after:bg-triangle after:lg:bg-no-repeat after:lg:block after:lg:left-[130px] after:lg:absolute after:lg:-bottom-6 relative ':'border-b-4 border-transparent'} pb-4 mb-12 `}>
+                  <div className="hidden capitalize lg:block">
+                  {dogCategory}
+                  </div>
+                </div>
               </div>
             );
           })}
         </div>
+        <Bundles  bundles={bundles}/>
       </div>
     </section>
   );
